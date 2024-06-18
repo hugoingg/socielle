d561d102174c7d2f53bd338594544b5c65d33b2f
Hugo Chen <l.hugochen@gmail.com>
2024-06-19 01:29:03 +0800

    fix: try adding p tags to iframe

--- src/pages/menu.tsx
+++ src/pages/menu.tsx
@@ -3,7 +3,12 @@ import "../css/menu.css";
 function MenuPage() {
   return (
     <div className="menu-container">
-      <iframe className="menu-book" src={menubook}></iframe>
+      <iframe className="menu-book" src={menubook}>
+        <p>
+          We apologize for the inconvenience, but there was an error. Please
+          view our menu <a href="google.com">here!</a>
+        </p>
+      </iframe>
     </div>
   );
 }
