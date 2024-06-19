dcaf8fca97dc35cc6f205dd5efe110f2f91f049b
Hugo Chen <l.hugochen@gmail.com>
2024-06-19 23:52:10 +0800

    add: room package

--- src/pages/menu.tsx
+++ src/pages/menu.tsx
@@ -1,15 +1,70 @@
-import menubook from "../media/menu book.pdf";
 import "../css/menu.css";
+import "../css/home.css";
+import "../css/aboutus.css";
+import menuh from "../media/menuh.jpg";
 function MenuPage() {
   return (
-    <div className="menu-container">
-      <iframe className="menu-book" src={menubook}>
-        <p>
-          We apologize for the inconvenience, but there was an error. Please
-          view our menu <a href="google.com">here!</a>
-        </p>
-      </iframe>
-    </div>
+    <>
+      <div className="about-us-header">
+        <img src={menuh}></img>
+        <div className="header-text">
+          <h1>Menu</h1>
+        </div>
+      </div>
+      <div className="menu-container">
+        <ul>
+          <li>
+            <a className="m-b" href="#menu">
+              <div className="block">
+                <h1>Menu</h1>
+              </div>
+            </a>
+          </li>
+          <li>
+            <hr />
+          </li>
+          <li>
+            <a className="rb-b" href="#booking">
+              <div className="block">
+                <h1>Booking</h1>
+              </div>
+            </a>
+          </li>
+        </ul>
+        <div className="segment" id="menu">
+          <h1 className="menu-segment">Our Menu</h1>
+          <hr></hr>
+          <br></br>
+          <iframe
+            allowFullScreen
+            className="fp-iframe"
+            src="https://heyzine.com/flip-book/7867e53c6a.html"
+            style={{
+              border: "0px",
+              width: "100%",
+              height: "333px",
+              overflow: "hidden",
+            }}
+          ></iframe>
+        </div>
+        <div className="segment" id="booking">
+          <h1 className="menu-segment">Room Booking</h1>
+          <hr></hr>
+          <br></br>
+          <iframe
+            allowFullScreen
+            className="fp-iframe"
+            src="https://heyzine.com/flip-book/975f728cf8.html"
+            style={{
+              border: "1px solid lightgray",
+              width: "100%",
+              height: "174px",
+              overflow: "hidden",
+            }}
+          ></iframe>
+        </div>
+      </div>
+    </>
   );
 }
 
